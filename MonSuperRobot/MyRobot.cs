cd283fd7ab5b0967b1874345b479fc9eb8a43291
Jeremy Fabre <jeremy.fabre@hotmail.com>
2011-03-28 19:59:37 -0400

    TDD rocode project

---
+++ MonSuperRobot/MyRobot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Robocode;
+
+namespace JFabre
+{
+    public class MyRobot : Robot
+    {
+        public override void Run()
+        {
+            // Perform your initialization for your robot here
+            while (true)
+            {
+                // Perform robot logic here calling robot commands etc.
+            }
+        }
+    }
+}
